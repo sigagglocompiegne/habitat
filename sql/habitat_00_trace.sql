f1b6c89eec98b773058ac72af676546ed374edee
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-28 17:13:57 +0100

    Update habitat_00_trace.sql

--- sql/habitat_00_trace.sql
+++ sql/habitat_00_trace.sql
@@ -1,1 +1,21 @@
+/*HABITAT V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* habitat_10_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+/*  
+  dépendances : voir script d'initialisation des dépendances init_bd_pei_dependencies.sql
+  
+  Liste des dépendances :
+  schéma          | table                 | description                                                   | usage
+
+*/
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+2018-12-28 : GB / initialisation du code
+
 
