1d5569a3bf62c7cb4b07abfff25e2803f89cb2fc
gregbodet <37414914+gregbodet@users.noreply.github.com>
2019-08-02 12:07:48 +0200

    Update habitat_00_trace.sql

--- sql/habitat_00_trace.sql
+++ sql/habitat_00_trace.sql
@@ -16,6 +16,6 @@
 
 /*
 #################################################################### SUIVI CODE SQL ####################################################################
-2019-07-01 : GB / initialisation du code uniquement pour la partie gestion Habitat indigne
+2019-08-02 : GB / initialisation du code uniquement pour la partie gestion Habitat indigne
 
 
