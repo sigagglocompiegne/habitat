74fde8a1a1d2bedd473c1f6befd50213ad3f772e
gregbodet <37414914+gregbodet@users.noreply.github.com>
2019-08-02 12:16:51 +0200

    Update habitat_10_squelette.sql

--- sql/habitat_10_squelette.sql
+++ sql/habitat_10_squelette.sql
@@ -6,6 +6,54 @@
 /* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
 /* Auteur : Grégory Bodet */
 
+
+-- ###############################################################################################################################
+-- ###                                                                                                                         ###
+-- ###                                                           DROP                                                          ###
+-- ###                                                                                                                         ###
+-- ###############################################################################################################################
+
+
+  -- fkey
+ALTER TABLE m_habitat.an_hab_indigne_occ DROP CONSTRAINT IF EXISTS an_hab_indigne_occ_pat_fkey;
+ALTER TABLE m_habitat.an_hab_indigne_occ DROP CONSTRAINT IF EXISTS an_hab_indigne_occ_situ_fkey;
+ALTER TABLE m_habitat.an_hab_indigne_prop DROP CONSTRAINT IF EXISTS an_hab_indigne_prop_pat_fkey;
+ALTER TABLE m_habitat.an_hab_indigne_prop DROP CONSTRAINT IF EXISTS an_hab_indigne_prop_tprop_fkey;
+ALTER TABLE m_habitat.an_hab_indigne_prop DROP CONSTRAINT IF EXISTS an_hab_indigne_prop_tvoie_fkey;
+ALTER TABLE m_habitat.an_hab_indigne_sign DROP CONSTRAINT IF EXISTS an_hab_indigne_sign_avancdos_fkey;
+ALTER TABLE m_habitat.an_hab_indigne_sign DROP CONSTRAINT IF EXISTS an_hab_indigne_sign_edos_fkey;
+ALTER TABLE m_habitat.an_hab_indigne_sign DROP CONSTRAINT IF EXISTS an_hab_indigne_sign_qualiffinal_fkey;
+ALTER TABLE m_habitat.an_hab_indigne_sign DROP CONSTRAINT IF EXISTS an_hab_indigne_sign_qualifinit_fkey;
+
+-- CLASSES
+
+DROP TABLE IF EXISTS m_habitat.an_hab_indigne_sign;
+DROP TABLE IF EXISTS m_habitat.an_hab_indigne_occ;
+DROP TABLE IF EXISTS m_habitat.an_hab_indigne_prop;
+DROP TABLE IF EXISTS m_habitat.an_hab_indigne_media;
+DROP TABLE IF EXISTS x_apps.xapps_an_v_hab_indigne_erreur;
+
+
+-- DOMAINES DE VALEUR
+
+DROP TABLE IF EXISTS m_habitat.x_apps.lt_hab_indigne_avancdos;
+DROP TABLE IF EXISTS m_habitat.x_apps.lt_hab_indigne_pat;
+DROP TABLE IF EXISTS m_habitat.x_apps.lt_hab_indigne_qualif;
+DROP TABLE IF EXISTS m_habitat.x_apps.lt_hab_indigne_situ;
+DROP TABLE IF EXISTS m_habitat.x_apps.lt_hab_indigne_tprop;
+
+--SEQUENCES
+
+DROP SEQUENCE IF EXISTS m_habitat.an_hab_indigne_media_gid_seq;
+DROP SEQUENCE IF EXISTS m_habitat.an_hab_indigne_occ_seq;
+DROP SEQUENCE IF EXISTS m_habitat.an_hab_indigne_prop_seq;
+DROP SEQUENCE IF EXISTS m_habitat.an_hab_indigne_sign_seq;
+DROP SEQUENCE IF EXISTS x_apps.xapps_an_v_hab_indigne_erreur_gid_seq;
+
+-- VUE APPLICATIVE
+
+DROP VIEW IF EXISTS x_apps.xapps_an_v_hab_indigne_tb1;
+
 -- ####################################################################################################################################################
 -- ###                                                                                                                                              ###
 -- ###                                                                SCHEMA                                                           ###
@@ -17,8 +65,7 @@
 -- DROP SCHEMA m_habitat;
 /*
 CREATE SCHEMA m_habitat
-  AUTHORIZATION sig_create;
-
+  AUTHORIZATION sig_create
 COMMENT ON SCHEMA m_habitat
   IS 'Données géographiques métiers sur le thème de l'habitat';
 */
