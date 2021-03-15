9aa58fb2b82c9f4afb5575b7657b7a076bb2f618
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-03-15 12:42:38 +0100

    Update habitat_99_grant.sql

--- sql/habitat_99_grant.sql
+++ sql/habitat_99_grant.sql
@@ -8,6 +8,9 @@
 
 
 
+-- ICI SONT PRESENTES LES DROITS DE MANIERES GENERIQUES COMME ILS DOIVENT ETRE INTEGRES POUR CHAQUE CLASSE D'OBJETS. SI DES PARTICULARITES SONT
+-- INTRODUITES ELLES SONT DETAILLEES CI-DESSOUS
+
 -- ####################################################################################################################################################
 -- ###                                                                                                                                              ###
 -- ###                                                                        GRANT                                                                  ###
@@ -15,150 +18,64 @@
 -- ####################################################################################################################################################
 
 
-
--- #################################################################### SCHEMA  ####################################################################
-
-GRANT ALL ON SCHEMA m_habitat TO sig_create;
-GRANT ALL ON SCHEMA m_habitat TO create_sig;
-GRANT USAGE ON SCHEMA m_habitat TO read_sig;
-GRANT USAGE ON SCHEMA m_habitat TO edit_sig;
-
--- #################################################################### DOMAINE DE VALEUR  ####################################################################
-
-ALTER TABLE m_habitat.lt_hab_indigne_qualif
-  OWNER TO sig_create;
-GRANT ALL ON TABLE m_habitat.lt_hab_indigne_qualif TO sig_create;
-GRANT ALL ON TABLE m_habitat.lt_hab_indigne_qualif TO create_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE m_habitat.lt_hab_indigne_qualif TO edit_sig;
-GRANT SELECT ON TABLE m_habitat.lt_hab_indigne_qualif TO read_sig;
-
-ALTER TABLE m_habitat.lt_hab_indigne_pat
-  OWNER TO sig_create;
-GRANT ALL ON TABLE m_habitat.lt_hab_indigne_pat TO sig_create;
-GRANT ALL ON TABLE m_habitat.lt_hab_indigne_pat TO create_sig;
-GRANT SELECT ON TABLE m_habitat.lt_hab_indigne_pat TO read_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE m_habitat.lt_hab_indigne_pat TO edit_sig;
-
-ALTER TABLE m_habitat.lt_hab_indigne_situ
-  OWNER TO sig_create;
-GRANT ALL ON TABLE m_habitat.lt_hab_indigne_situ TO sig_create;
-GRANT ALL ON TABLE m_habitat.lt_hab_indigne_situ TO create_sig;
-GRANT SELECT ON TABLE m_habitat.lt_hab_indigne_situ TO read_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE m_habitat.lt_hab_indigne_situ TO edit_sig;
-
-ALTER TABLE m_habitat.lt_hab_indigne_tprop
-  OWNER TO sig_create;
-GRANT ALL ON TABLE m_habitat.lt_hab_indigne_tprop TO sig_create;
-GRANT ALL ON TABLE m_habitat.lt_hab_indigne_tprop TO create_sig;
-GRANT SELECT ON TABLE m_habitat.lt_hab_indigne_tprop TO read_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE m_habitat.lt_hab_indigne_tprop TO edit_sig;
-
-ALTER TABLE m_habitat.lt_hab_indigne_avancdos
-  OWNER TO sig_create;
-GRANT ALL ON TABLE m_habitat.lt_hab_indigne_avancdos TO sig_create;
-GRANT ALL ON TABLE m_habitat.lt_hab_indigne_avancdos TO create_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE m_habitat.lt_hab_indigne_avancdos TO edit_sig;
-GRANT SELECT ON TABLE m_habitat.lt_hab_indigne_avancdos TO read_sig;
-
-
 -- #################################################################### SEQUENCE  ####################################################################
 
-ALTER TABLE m_habitat.an_hab_indigne_sign_seq
-  OWNER TO sig_create;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_sign_seq TO sig_create;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_sign_seq TO public;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_sign_seq TO create_sig;
+ALTER SEQUENCE [schema].[sequence]
+    OWNER TO create_sig;
 
-ALTER TABLE m_habitat.an_hab_indigne_occ_seq
-  OWNER TO sig_create;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_occ_seq TO sig_create;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_occ_seq TO public;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_occ_seq TO create_sig;
+GRANT ALL ON SEQUENCE [schema].[sequence] TO create_sig;
+    
+-- #################################################################### DOMAINE DE VALEUR  ####################################################################
 
-ALTER TABLE m_habitat.an_hab_indigne_prop_seq
-  OWNER TO sig_create;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_prop_seq TO sig_create;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_prop_seq TO public;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_prop_seq TO create_sig;
+ALTER TABLE [schema].[table]
+    OWNER to create_sig;
 
-ALTER TABLE m_habitat.an_hab_indigne_media_gid_seq
-  OWNER TO sig_create;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_media_gid_seq TO sig_create;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_media_gid_seq TO public;
-GRANT ALL ON SEQUENCE m_habitat.an_hab_indigne_media_gid_seq TO create_sig;
+GRANT ALL ON TABLE [schema].[table] TO sig_create;
+GRANT SELECT ON TABLE [schema].[table] TO sig_read;
+GRANT ALL ON TABLE [schema].[table] TO create_sig;
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE [schema].[table] TO sig_edit;
 
-ALTER TABLE x_apps.xapps_an_v_hab_indigne_erreur_gid_seq
-  OWNER TO sig_create;
-GRANT ALL ON SEQUENCE x_apps.xapps_an_v_hab_indigne_erreur_gid_seq TO sig_create;
-GRANT ALL ON SEQUENCE x_apps.xapps_an_v_hab_indigne_erreur_gid_seq TO public;
-GRANT ALL ON SEQUENCE x_apps.xapps_an_v_hab_indigne_erreur_gid_seq TO create_sig;
+-- #################################################################### CLASSE D'OBJET ####################################################################
 
 
+ALTER TABLE [schema].[table]
+    OWNER to create_sig;
 
--- #################################################################### TABLE  ####################################################################
+GRANT ALL ON TABLE [schema].[table] TO sig_create;
+GRANT SELECT ON TABLE [schema].[table] TO sig_read;
+GRANT ALL ON TABLE [schema].[table] TO create_sig;
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE [schema].[table] TO sig_edit;
 
-ALTER TABLE m_habitat.an_hab_indigne_sign
-  OWNER TO sig_create;
-GRANT ALL ON TABLE m_habitat.an_hab_indigne_sign TO sig_create;
-GRANT ALL ON TABLE m_habitat.an_hab_indigne_sign TO create_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE m_habitat.an_hab_indigne_sign TO edit_sig;
-GRANT SELECT ON TABLE m_habitat.an_hab_indigne_sign TO read_sig;
 
-ALTER TABLE m_habitat.an_hab_indigne_occ
-  OWNER TO sig_create;
-GRANT ALL ON TABLE m_habitat.an_hab_indigne_occ TO sig_create;
-GRANT ALL ON TABLE m_habitat.an_hab_indigne_occ TO create_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE m_habitat.an_hab_indigne_occ TO edit_sig;
-GRANT SELECT ON TABLE m_habitat.an_hab_indigne_occ TO read_sig;
+-- ########################################################### FONCTION  ####################################################################
 
-ALTER TABLE m_habitat.an_hab_indigne_prop
-  OWNER TO sig_create;
-GRANT ALL ON TABLE m_habitat.an_hab_indigne_prop TO sig_create;
-GRANT ALL ON TABLE m_habitat.an_hab_indigne_prop TO create_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE m_habitat.an_hab_indigne_prop TO edit_sig;
-GRANT SELECT ON TABLE m_habitat.an_hab_indigne_prop TO read_sig;
+ALTER FUNCTION [schema].[fonction]()
+    OWNER TO create_sig;
 
-ALTER TABLE m_habitat.an_hab_indigne_media
-  OWNER TO sig_create;
-GRANT ALL ON TABLE m_habitat.an_hab_indigne_media TO sig_create;
-GRANT ALL ON TABLE m_habitat.an_hab_indigne_media TO create_sig;
-GRANT SELECT ON TABLE m_habitat.an_hab_indigne_media TO read_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE m_habitat.an_hab_indigne_media TO edit_sig;
+GRANT EXECUTE ON FUNCTION [schema].[fonction]() TO PUBLIC;
+GRANT EXECUTE ON FUNCTION [schema].[fonction]() TO create_sig;
 
-ALTER TABLE x_apps.xapps_an_v_hab_indigne_erreur
-  OWNER TO sig_create;
-GRANT ALL ON TABLE x_apps.xapps_an_v_hab_indigne_erreur TO sig_create;
-GRANT ALL ON TABLE x_apps.xapps_an_v_hab_indigne_erreur TO create_sig;
-GRANT SELECT ON TABLE x_apps.xapps_an_v_hab_indigne_erreur TO read_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE x_apps.xapps_an_v_hab_indigne_erreur TO edit_sig;
 
--- #################################################################### VUE DE GESTION  ####################################################################
+-- ########################################################### VUE DE GESTION  ####################################################################
 
-Sans objet
+ALTER TABLE [schema].[vue]
+OWNER TO create_sig;
 
--- #################################################################### VUE APPLICATIVE  ####################################################################
+GRANT ALL ON TABLE [schema].[vue] TO sig_create;
+GRANT SELECT ON TABLE [schema].[vue] TO sig_read;
+GRANT ALL ON TABLE [schema].[vue] TO create_sig;
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE [schema].[vue] TO sig_edit;
 
-ALTER TABLE x_apps.xapps_an_v_hab_indigne_tb1
-  OWNER TO sig_create;
-GRANT ALL ON TABLE x_apps.xapps_an_v_hab_indigne_tb1 TO sig_create;
-GRANT ALL ON TABLE x_apps.xapps_an_v_hab_indigne_tb1 TO create_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE x_apps.xapps_an_v_hab_indigne_tb1 TO edit_sig;
-GRANT SELECT ON TABLE x_apps.xapps_an_v_hab_indigne_tb1 TO read_sig;
 
-ALTER TABLE x_apps.xapps_geo_v_hab_indigne_delais
-  OWNER TO sig_create;
-GRANT ALL ON TABLE x_apps.xapps_geo_v_hab_indigne_delais TO sig_create;
-GRANT ALL ON TABLE x_apps.xapps_geo_v_hab_indigne_delais TO create_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE x_apps.xapps_geo_v_hab_indigne_delais TO edit_sig;
-GRANT SELECT ON TABLE x_apps.xapps_geo_v_hab_indigne_delais TO read_sig;
+-- ########################################################### VUE APPLICATIVE ou OPEN DATA ####################################################################
 
-ALTER TABLE x_apps.xapps_geo_v_hab_indigne
-  OWNER TO sig_create;
-GRANT ALL ON TABLE x_apps.xapps_geo_v_hab_indigne TO sig_create;
-GRANT ALL ON TABLE x_apps.xapps_geo_v_hab_indigne TO create_sig;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE x_apps.xapps_geo_v_hab_indigne TO edit_sig;
-GRANT SELECT ON TABLE x_apps.xapps_geo_v_hab_indigne TO read_sig;
+ALTER TABLE [schema].[vue]
+    OWNER TO create_sig;
 
+GRANT SELECT ON TABLE [schema].[vue] TO sig_create;
+GRANT SELECT ON TABLE [schema].[vue] TO sig_read;
+GRANT SELECT ON TABLE [schema].[vue] TO sig_edit;
+GRANT SELECT ON TABLE [schema].[vue] TO create_sig;
 -- #################################################################### VUE OPENDATA  ####################################################################
 
 
